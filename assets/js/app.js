4ef4a1351617de03651287483f9d270d04113b24
Thibaud Saint-Etienne <saint.etienne.thibaud@gmail.com>
2018-04-07 19:04:56 +0200

    decode image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,28 @@
 class Stego {
 
   constructor(image) {
-    this.text_in_image("Can you decode myself ?");
+    this.text_in_image(`[Intro : Kwest OG]
+    Volume 2, Kwest OG
+    
+    [Couplet unique : Kwest OG & Cadreur]
+    Grigny-la-Grande-Borne, une bite sur l'épaule
+    3 cadavres, sol, quartier mahbol
+    Face à l'horreur, nous sommes
+    Nous sommes, des tueurs
+    Général de guerre et casquette à l'envers
+    Les pecs en l'air, le bras à Schwarzennegger
+    Tu gazes on t'monte en l'air pour des billets, (ttt)-verts
+    Je suis un braqueur, fuck le procureur !
+    Fuck le procureur ! Fuck ? Le procureur
+    
+    [Outro : Kwest OG]
+    Bien joué messieurs dames`);
   }
 
   text_in_image(text) {
     var array = this.encode_image(this.text_to_binary(text), image);
     this.redo_image_from_pixelArray(this.binArray_to_RGBArray(array));
+    this.decode_image()
   }
 
   text_to_binary(text) {
@@ -22,7 +38,7 @@ class Stego {
     return output;
   }
 
-  binary_to_text(text) {
+  static binary_to_text(text) {
     var output = '';
     
     text.match(/.{8}/g).map(function(bin) {
@@ -31,7 +47,7 @@ class Stego {
     return output;
   }
 
-  image_to_binary(image) {
+  static image_to_binary(image) {
     var binArray = [];
     
     var canvas = document.createElement('canvas');
@@ -58,7 +74,7 @@ class Stego {
   }
 
   encode_image(text, image) {
-    var binArrayImage = this.image_to_binary(image);
+    var binArrayImage = Stego.image_to_binary(image);
     for (let i = 0; i < text.length; i++) {
       var charToAdd = text.charAt(i);
       binArrayImage[i] = binArrayImage[i].slice(0, -1) + charToAdd;
@@ -66,8 +82,19 @@ class Stego {
     return binArrayImage;
   }
 
-  decode_image(image) {
-    
+  decode_image() {
+    var c = document.getElementById("myCanvas");
+    var image = this.convertCanvasToImage(c);
+    image.onload = function() {
+      var binArray = Stego.image_to_binary(image);
+      var input = "";
+      var output = "";
+      for (let i = 0; i < binArray.length; i++) {
+        input += binArray[i].substr(binArray[i].length - 1);
+      }
+      output = Stego.binary_to_text(input);
+      console.log(output);
+    }
   }
 
   binArray_to_RGBArray(binArray) {
@@ -88,9 +115,20 @@ class Stego {
         imgData.data[i+0] = concatArray[i+0];
         imgData.data[i+1] = concatArray[i+1];
         imgData.data[i+2] = concatArray[i+2];
-        imgData.data[i+3] = 255;
+        imgData.data[i+3] = concatArray[i+3];
     }
     ctx.putImageData(imgData, 10, 10);
+  }
+
+  download_image() {
+    var canvas = document.getElementById("myCanvas");
+    document.location.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+  }
+
+  convertCanvasToImage(canvas) {
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+    return image;
   }
 
 }
